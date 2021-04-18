e44a56fa2e18dbd8d88b217a7efab3035389bdaa
umigame4031 <unun.octium1031@gmail.com>
2021-04-18 17:55:56 +0900

    postsテーブルのマイグレーションファイル記述

--- db/migrate/20210418041602_create_posts.rb
+++ db/migrate/20210418041602_create_posts.rb
@@ -1,7 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.
+      t.date    :date,    null: false
+      t.integer :area_id, null: false
+      t.string  :field,   null: false
+      t.integer :fish_id, null: false
+      t.string  :tackle,  null: false
+      t.text    :text,    null: false
       t.timestamps
     end
   end
