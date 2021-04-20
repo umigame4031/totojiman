b7d35233932d358d182c6f3b73042baf7f30f5db
umigame4031 <unun.octium1031@gmail.com>
2021-04-20 23:22:13 +0900

    トップページへのルーティング

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :area
 
-  validates :date, :field, :fish, :tackle, :text, presence: true
+  has_one_attached :image
 
+  validates :date, :field, :fish, :tackle, :text, presence: true
   validates :genre_id, numericality: { other_than: 1 } 
 end
