b04908c7578da5849593091a3771f962c31eb388
umigame4031 <unun.octium1031@gmail.com>
2021-04-29 14:03:59 +0900

    createアクション

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :date, :area_id, :field, :genre_id, :fish, :tackle, :text, :image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :new]
+  resources :posts, only: [:index, :new, :create]
   root to: 'posts#new'
 end
