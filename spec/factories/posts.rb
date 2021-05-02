6ef8cc8c6324a4cf648f8162e30691afbce1a297
umigame4031 <unun.octium1031@gmail.com>
2021-05-02 17:47:06 +0900

    post factorybot

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :post do
-    
+    title { Faker::Lorem.sentence }
+    date { Faker::Date.in_date_period }
+    area_id { Faker::Number.between(from: 2, to: 48) }
+    field { Faker::Lorem.sentence }
+    genre_id { Faker::Number.between(from: 2, to: 14) }
+    fish { 'ブラックバス' }
+    tackle { Faker::Lorem.sentence }
+    text { Faker::Lorem.sentence }
+    # association :user 
+
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @post = FactoryBot.build(:post)
+    end
+
+    context '釣果投稿ができるとき' do
+      it 'title,date,area_id,field,genre_id,fish,tackle,text,imageが存在すれば投稿できる' do
+        
+      end
+    end
+
+    context '釣果投稿ができないとき' do
+
+    end
+  end
 end
