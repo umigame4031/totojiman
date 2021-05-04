8d56439e136d0a1770683d4584942587c7fc36ad
umigame4031 <unun.octium1031@gmail.com>
2021-05-04 23:22:22 +0900

    投稿詳細ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create, :show]
   root to: 'posts#index'
 end
