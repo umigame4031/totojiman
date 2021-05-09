692ebb796e96c50809b5e27627743b30de68eee1
umigame4031 <unun.octium1031@gmail.com>
2021-05-09 22:32:06 +0900

    orderテーブルマイグレーションファイル記述

--- db/migrate/20210509013607_create_orders.rb
+++ db/migrate/20210509013607_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      
+      t.references :client, foreign_key: { to_table: :users }
+      t.references :contractor, foreign_key: { to_table: :users }
       t.timestamps
     end
   end
