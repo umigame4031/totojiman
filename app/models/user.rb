e828b846831c0d715ccf0d2f402528870b03c094
umigame4031 <unun.octium1031@gmail.com>
2021-05-11 23:04:37 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts
+  has_many :clients, class_name: "Order", foreign_key: "client_id"
+  has_many :contractors, class_name: "Order", foreign_key: "contractor_id"
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
