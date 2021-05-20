8d1eb9d44f1bfe205d4270713decbdcaad2e8705
umigame4031 <unun.octium1031@gmail.com>
2021-05-21 01:04:05 +0900

    dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   has_many :orders
   has_many :client_orders, class_name: "Order", :foreign_key => 'client_id'
