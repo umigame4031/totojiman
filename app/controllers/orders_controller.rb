c9d88f883f773dca4304d1a789c7c4f9ea6b53b0
umigame4031 <unun.octium1031@gmail.com>
2021-05-22 23:12:26 +0900

    ガイド依頼機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,14 @@ class OrdersController < ApplicationController
   def index
     @post = Post.find(params[:post_id])
   end
+
+  def create
+    @order = Order.new(order_params)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:hour).merge(client_id: current_user.id, contractor_id: @post.user.id )
+  end
 end
