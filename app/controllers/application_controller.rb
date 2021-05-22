565924e62a9f6a5f808402edbdeb8c3c9ce6dacc
umigame4031 <unun.octium1031@gmail.com>
2021-05-22 16:16:59 +0900

    ガイド料金ユーザー登録画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :guide_permit, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :guide_permit, :image, :guide_fee])
   end
 end
