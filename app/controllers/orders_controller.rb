6120956281d09c5ab4b610045cf0524ed8ddb9c0
umigame4031 <unun.octium1031@gmail.com>
2021-06-02 23:17:20 +0900

    amount変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,7 @@ class OrdersController < ApplicationController
     if @order.valid?
       Payjp.api_key = ""  # 自身のPAY.JPテスト秘密鍵を記述しましょう
       Payjp::Charge.create(
-        amount: order_params[:price],  # 商品の値段
+        amount: ,  # 商品の値段
         card: order_params[:token],    # カードトークン
         currency: 'jpy'                 # 通貨の種類（日本円）
       )
