42780d8d68a575b011e55df5ee509001ec15b2c9
umigame4031 <unun.octium1031@gmail.com>
2021-06-07 23:16:22 +0900

    メモ書き削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,11 +8,11 @@ class OrdersController < ApplicationController
     @post = Post.find(params[:post_id])
     @order = Order.new(order_params)
     if @order.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
       Payjp::Charge.create(
-        amount: @post.user.guide_fee ,  # 商品の値段
-        card: order_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
+        amount: @post.user.guide_fee , 
+        card: order_params[:token],  
+        currency: 'jpy' 
       )
       @order.save
       redirect_to root_path
